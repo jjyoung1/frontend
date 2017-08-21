0e1d5c20810802f14c5263dc066f28d1f7ac5206
Joel Young <joel@jyng.com>
2017-08-21 15:29:03 -0400

    Simplified JS for NYT

--- js/script.js
+++ js/script.js
@@ -19,10 +19,13 @@ function loadData() {
     $body.append('<img class="bgimg" src=' + imgSrc + '/>');
 
     // New York Times
-    let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-    url += $.param({
-        'api_key': "cf9c7b4041244e59b05931de84704d5e"
-    });
+    let url = 'https://api.nytimes.com/svc/search/v2/' +
+        'articlesearch.json?q=' + cityStr +
+        '&sort=newest&api-key=cf9c7b4041244e59b05931de84704d5e';
+
+    // url += $.param({
+    //     'api_key': "cf9c7b4041244e59b05931de84704d5e"
+    // });
 
     $.getJSON(url, function (data) {
         let docs;
