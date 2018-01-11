0a9525838c1634aab92f254c2c2cf7d4480664af
Joel Young <joel@jyng.com>
2018-01-11 14:10:11 -0500

    Added capability for multiple cats

--- catclicker/js/script.js
+++ catclicker/js/script.js
@@ -1,14 +1,64 @@
-(function(){
-    var $counter = $('#clickcounter');
-    var click_count = 0;
+(function () {
+    var cats = [
+        {
+            "name": "cat1",
+            "photo": "cat1.jpg",
+            "counter": 0
+        },
+        {
+            "name": "cat2",
+            "photo": "cat2.jpg",
+            "counter": 0
+        }
+    ];
 
-    function increment_counter(){
 
-        click_count++;
-        $counter[0].innerHTML = click_count.toString();
+    function getCounterStr(c) {
+        return ("Current click count: " + c.toString());
+    }
+    function increment_counter(i) {
+
+        cats[i]['counter']++;
+        $counter = document.getElementById('counter_cat'+i.toString());
+        $counter.innerHTML = getCounterStr(cats[i]["counter"].toString());
     }
 
-    $('#cat').click(function(e){
-        increment_counter();
-    })
+    // $('#cat').click(function (e) {
+    //     increment_counter();
+    // });
+
+    // Add cats to the page
+    for (var i = 0; i < cats.length; i++) {
+        // Create a new div for each cat
+        newCatDiv = document.createElement("div");
+        newCatDiv.setAttribute('id', 'cat' + i);
+
+        // Add cat's name to div
+        newCatName = document.createElement("h2");
+        newCatName.innerHTML = cats[i].name;
+        newCatDiv.append(newCatName);
+
+        // Add Cat Photo to div
+        newCatPhoto = document.createElement("img");
+        newCatPhoto.setAttribute('id', 'catphoto' + i.toString())
+        newCatPhoto.setAttribute("src", "image/" + cats[i].photo);
+        newCatPhoto.setAttribute('alt', "Cat's name is " + cats[i].name);
+
+        // Add cat counter event handler
+        newCatPhoto.addEventListener("click", (function(i) {
+            return function(){
+                increment_counter(i);
+            }
+        })(i));
+        newCatDiv.append(newCatPhoto);
+
+        newCatVoteCount = document.createElement("p");
+        newCatVoteCount.setAttribute('id','counter_cat'+i.toString());
+        newCatVoteCount.innerHTML = getCounterStr(cats[i].counter);
+        newCatDiv.append(newCatVoteCount);
+
+
+        // Add new cat to cats div
+        $("#cats").append(newCatDiv);
+    }
 })();
