431bceaa83e1b719462742c51932e7dcc6fd7bdc
Joel Young <joel@jyng.com>
2018-01-15 16:07:56 -0500

    Added links which select cat to display in main window.  Separated menu and main views

--- catclicker/js/script.js
+++ catclicker/js/script.js
@@ -1,12 +1,12 @@
 (function () {
-    var current_cat;
-
     var model = {
+        current_cat: null,
+
         cats: [
             {
                 "name": "cat1",
                 "photo": "image/cat1.jpg",
-                "counter": 0,
+                "counter": 0
             },
             {
                 "name": "cat2",
@@ -17,59 +17,117 @@
                 "name": "cat3",
                 "photo": "image/cat3.jpg",
                 "counter": 0
+            },
+            {
+                "name": "cat4",
+                "photo": "image/cat4.jpg",
+                "counter": 0
+            },
+            {
+                "name": "cat5",
+                "photo": "image/cat5.jpeg",
+                "counter": 0
+            },
+            {
+                "name": "cat6",
+                "photo": "image/cat6.jpeg",
+                "counter": 0
+            },
+            {
+                "name": "cat7",
+                "photo": "image/cat7.jpeg",
+                "counter": 0
+            },
+            {
+                "name": "cat8",
+                "photo": "image/cat3.jpg",
+                "counter": 0
             }
-        ],
-
-        getCounter: function (i) {
-            return (cats[i].counter);
-        },
-
-        increment_counter: function (i) {
-            cats[i].counter++;
-            return getCounter(i)
-        }
-
+        ]
     };
 
     var controller = {
-        current_cat: 0,
+
 
         getCats: function () {
             return model.cats;
         },
 
+        getCurrentCat: function () {
+            return model.current_cat;
+        },
+
+        setCurrentCat: function (cat) {
+            model.current_cat = cat;
+            cat_view.render();
+        },
+
         init: function () {
-            view.init();
+            model.current_cat = model.cats[0];
+
+            nav_view.init();
+            cat_view.init();
         }
     };
 
 
-    var view = {
+    var nav_view = {
+
         init: function () {
-            // Get elements for use in render function
-            this.$catButtons = $("div#catbuttons");
-            this.$catCandidate = $("div#cat-candidate");
-            this.$catbuttonTemplate = $('script[data-template="cat-button"]').html();
+            // Setup reference to the catlist
+            this.catListElem = document.getElementById('catlist');
+
+            // render the view
+            this.render();
+        },
+
+        render: function () {
+            let cat;
+            let cats = controller.getCats();
+
+            // Clear out the cat list
+            this.catListElem.innerHTML = "";
+
+            // Populate the buttons in the view
+            for (i = 0; i < cats.length; i++) {
+                cat = cats[i];
+                listElem = document.createElement("li");
+                listElem.innerHTML = cat.name;
+
+                listElem.addEventListener("click", (function (cat) {
+                    return function () {
+                        controller.setCurrentCat(cat);
+                    };
+                })(cat));
+                this.catListElem.append(listElem);
+            }
+        }
+    };
+
+
+    var cat_view = {
+
+        init: function () {
+            // Get element references for view
+            this.catNameElem = document.getElementById('cat-name');
+            this.catCountElem = document.getElementById('cat-count');
+            this.catImgElem = document.getElementById('cat-image');
+
+            // Attach event listener to image
+            this.catImgElem.addEventListener("click", function () {
+                cat = controller.getCurrentCat();
+                cat.counter++;
+                cat_view.render();
+            });
 
             this.render()
         },
+
         render: function () {
-            $("#cat-candidate").html("<h2>It Works</h2>");
-
-            // Clear navbar
-            this.$catButtons.html('');
-
-            // Clear cat candidate
-            this.$catCandidate.html('');
-
-            // Add Button for each cat
-            cats = controller.getCats();
-            for (let i = 0; i < cats.length; i++) {
-                let cbt = this.$catbuttonTemplate;
-                let tb = cbt.replace(/{{catid}}/g, i.toString());
-                tb = tb.replace(/{{catname}}/g, cats[i].name);
-                this.$catButtons.append(tb);
-            }
+            cat = controller.getCurrentCat()
+            this.catNameElem.innerText = cat.name;
+            this.catCountElem.innerText = cat.counter;
+            this.catImgElem.src = cat.photo;
         }
     };
 
